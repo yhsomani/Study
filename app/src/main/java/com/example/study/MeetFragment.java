585a452465aea8b5df024f7bd749ba0eeb79e3ca
ysoma <ysomani07@gmail.com>
2021-11-24 20:17:11 +0530

    Some UI Changes

--- app/src/main/java/com/example/study/MeetFragment.java
+++ app/src/main/java/com/example/study/MeetFragment.java
@@ -1,5 +1,6 @@
 package com.example.study;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MeetFragment extends Fragment {
+    EditText secretCodeBox;
+    Button joinBtn, shareBtn, createBtn;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +63,24 @@ public class MeetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_meet, container, false);
+        secretCodeBox =view.findViewById(R.id.secretCodeBox);
+        createBtn = view.findViewById(R.id.createBtn);
+        joinBtn = view.findViewById(R.id.joinBtn);
+        shareBtn = view.findViewById(R.id.shareBtn);
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+                String shareBody = "Required Meeting Code: "+ secretCodeBox.getText().toString();
+                intent.setType("text/plain");
+//                intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Study");
+                intent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meet, container, false);
+        return view;
     }
 }
